813423408cea4083546af57348f7a2290c209593
sin13cos14 <sin13cos14@gmail.com>
2016-10-13 01:13:04 +0800

    Scala Class

---
+++ src/main/scala/day2/Person.scala
@@ -0,0 +1,23 @@
+package day2
+
+/**
+  * Created by sin13cos14 on 16/10/13  01:06.
+  */
+class Person(val firstName: String, val lastName: String) {
+  private var position: String = _
+  println("Creating " + toString())
+
+  def this(firstName: String, lastName: String, positionHeld: String) {
+    this(firstName, lastName)
+    position = positionHeld
+  }
+
+  override def toString: String = {
+    firstName + " " + lastName + " holds" + position + " position"
+  }
+}
+
+// val jack = new Person("sin13","cos14","DWS")
+// println(john)
+// val bill = new Person("Bill","Walker")
+// println(bill)
