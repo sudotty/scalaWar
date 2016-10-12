a31f9dfbc8858fa214717b0abffec9ce05b6a82a
sin13cos14 <sin13cos14@gmail.com>
2016-10-13 01:17:22 +0800

    Scala Class

--- src/main/scala/day2/Person.scala
+++ src/main/scala/day2/Person.scala
@@ -1,10 +1,13 @@
 package day2
 
+import scala.beans.BeanProperty
+
 /**
   * Created by sin13cos14 on 16/10/13  01:06.
   */
 class Person(val firstName: String, val lastName: String) {
   private var position: String = _
+  @BeanProperty  var age:Int = _
   println("Creating " + toString())
 
   def this(firstName: String, lastName: String, positionHeld: String) {
