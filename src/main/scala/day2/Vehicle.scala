57e36530698ee9ccef24093247597fc5e4b56d86
sin13cos14 <sin13cos14@gmail.com>
2016-10-13 01:28:11 +0800

    Scala Class extends

---
+++ src/main/scala/day2/Vehicle.scala
@@ -0,0 +1,15 @@
+package day2
+
+/**
+  * Created by sin13cos14 on 16/10/13  01:21.
+  */
+class Vehicle(val id: Int, val year: Int) {
+  override def toString: String = "ID" + id + "Year:" + year
+}
+
+class Car(override val id: Int, override val year: Int, var fueLevel: Int) extends Vehicle(id, year) {
+  override def toString: String = super.toString + " Fuel Level:" + fueLevel
+}
+
+// val car = new Car(1,2009,100)
+// println(car)
