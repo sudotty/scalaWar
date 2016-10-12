9ec1f7efc22112bcbfc765d785c29f10fa939f6b
sin13cos14 <sin13cos14@gmail.com>
2016-10-13 00:13:33 +0800

    Complex and Equality

--- src/main/scala/day1/Complex.scala
+++ src/main/scala/day1/Complex.scala
@@ -8,6 +8,10 @@ class Complex(val real: Int, val imaginary: Int) {
     new Complex(real + operand.real, imaginary + operand.imaginary)
   }
 
+  def *(operand: Complex): Complex = {
+    new Complex(real * operand.real - imaginary * operand.imaginary, real * operand.imaginary + imaginary * operand.real)
+  }
+
   override def toString: String = {
     real + (if (imaginary < 0) "" else "+") + imaginary + "i"
   }
@@ -16,3 +20,4 @@ class Complex(val real: Int, val imaginary: Int) {
 //val c1 = new Complex(1,2)
 //val c2 = new Complex(2,-3)
 //val sum = c1+c2
+//val multiple = c1*c2
---
+++ src/main/scala/day1/Equality.scala
@@ -0,0 +1,13 @@
+/**
+  * Created by sin13cos14 on 16/10/13  00:06.
+  */
+
+val str1 = "Hello"
+val str2 = "Hello"
+var str3 = new String("Hello")
+
+println(str1 == str2)
+println(str1 eq str2)
+
+println(str1 == str3)
+println(str1 eq str3)
