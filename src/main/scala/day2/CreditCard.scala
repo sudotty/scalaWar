1d5d72d6d197f89dd43a0ac1c95c7abf98624ba7
sin13cos14 <sin13cos14@gmail.com>
2016-10-13 01:04:55 +0800

    Scala Class

---
+++ src/main/scala/day2/CreditCard.scala
@@ -0,0 +1,9 @@
+package day2
+
+/**
+  * Created by sin13cos14 on 16/10/13  00:55.
+  */
+class CreditCard(val number: Int, var creditLimit: Int)
+
+//scalac CreditCard
+//javap -private day2.CreditCard
