3669fd1082343191e1e6d278505bbf2d0887fc53
sin13cos14 <sin13cos14@gmail.com>
2016-10-12 23:40:16 +0800

    Complex

---
+++ src/main/scala/day1/Complex.scala
@@ -0,0 +1,18 @@
+package day1
+
+/**
+  * Created by sin13cos14 on 16/10/12  23:31.
+  */
+class Complex(val real: Int, val imaginary: Int) {
+  def +(operand: Complex): Complex = {
+    new Complex(real + operand.real, imaginary + operand.imaginary)
+  }
+
+  override def toString: String = {
+    real + (if (imaginary < 0) "" else "+") + imaginary + "i"
+  }
+}
+
+//val c1 = new Complex(1,2)
+//val c2 = new Complex(2,-3)
+//val sum = c1+c2
