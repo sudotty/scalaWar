d0b1570b0533db0bf7fb075b12f36611911ac7e2
sin13cos14 <sin13cos14@gmail.com>
2016-10-13 00:17:55 +0800

    Access

---
+++ src/main/scala/day1/Access.scala
@@ -0,0 +1,14 @@
+/**
+  * Created by sin13cos14 on 16/10/13  00:13.
+  */
+class Microwave {
+  def start() = println("start")
+
+  def stop() = println("stop")
+
+  private def turnTable() = println("turning table")
+}
+
+val microwave = new Microwave
+microwave.start()
+//microwave.turnTable //Error
