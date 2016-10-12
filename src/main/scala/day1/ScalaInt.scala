99ea638a8e28579af8b9bda24177730e4c976e21
sin13cos14 <sin13cos14@gmail.com>
2016-10-12 23:12:08 +0800

    scala Rich Int

---
+++ src/main/scala/day1/ScalaInt.scala
@@ -0,0 +1,14 @@
+package day1
+
+import java.util
+
+/**
+  * Created by sin13cos14 on 16/10/12  23:05.
+  */
+class ScalaInt {
+  def playWithInt() {
+    val capacity: Int = 10
+    val list = new util.ArrayList[String]()
+    list.ensureCapacity(capacity)
+  }
+}
