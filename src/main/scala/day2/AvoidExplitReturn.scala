a4cac2364e684f8bd35f02207ffeb11240741c90
sin13cos14 <sin13cos14@gmail.com>
2016-10-14 23:21:54 +0800

    Scala singleton

--- src/main/scala/day2/AvoidExplitReturn.scala
+++ src/main/scala/day2/AvoidExplitReturn.scala
@@ -5,7 +5,7 @@ class AvoidExplicitReturn {
   def check1() = true
 
   def check2(): Boolean = return true //有return,那么就显示的指定返回类型,最好不加,scala会自动推断
-  println(check1)
-  println(check2)
+  println(check1())
+  println(check2())
 
 }
