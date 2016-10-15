826b714249c9c65f0f7a2c3a24a7f6f5a78b39dc
sin13cos14 <sin13cos14@gmail.com>
2016-10-15 22:49:04 +0800

    Scala singleton

---
+++ src/main/scala/day3/Marker.scala
@@ -0,0 +1,8 @@
+/**
+  * Created by sin13cos14 on 16/10/15  22:47.
+  */
+class Marker private(val color: String) {
+  println("Creating" + this)
+
+  override def toString: String = "marker color " + color
+}
