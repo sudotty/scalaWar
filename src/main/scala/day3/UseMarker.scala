4b4bb1cd51096bc1586a3611fcba168edd6ade02
sin13cos14 <sin13cos14@gmail.com>
2016-10-15 22:53:52 +0800

    Scala singleton

--- src/main/scala/day3/UseMarker.scala
+++ src/main/scala/day3/UseMarker.scala
@@ -1,7 +1,11 @@
 /**
   * Created by sin13cos14 on 16/10/15  22:52.
   */
-println(Marker getMarker "red")
-println(Marker getMarker "red")
-println(Marker getMarker "blue")
-println(Marker getMarker "blue")
+
+class UseMarker {
+  println(Marker getMarker "red")
+  println(Marker getMarker "red")
+  println(Marker getMarker "blue")
+  println(Marker getMarker "blue")
+
+}
