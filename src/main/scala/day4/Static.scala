593c12569e050a627b5ddabab3e57d7e7c76c87c
sin13cos14 <sin13cos14@gmail.com>
2016-10-16 23:04:12 +0800

    Scala static

--- src/main/scala/day4/Static.scala
+++ src/main/scala/day4/Static.scala
@@ -1,4 +1,14 @@
 /**
   * Created by sin13cos14 on 16/10/16  23:02.
   */
+class Marker private(val color: String) {
+  override def toString: String = "marker color " + color
+}
+object Marker{
+  private val markers = Map(
+    "red"->new Marker("red"),
+    "blue"->new Marker("blue"),
+    "green"->new Marker("green")
+  )
 
+}
