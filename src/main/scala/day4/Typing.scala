7a76ddbf837659e32c88aa1bf873fad99e1ba423
sin13cos14 <sin13cos14@gmail.com>
2016-10-16 23:15:51 +0800

    Scala type

--- src/main/scala/day4/Typing.scala
+++ src/main/scala/day4/Typing.scala
@@ -4,5 +4,9 @@ package day4
   * Created by sin13cos14 on 16/10/16  23:12.
   */
 class Typing {
-
+  var year: Int = 2016
+  var anotherYear = 2016
+  var greet = "Hello there"
+  var builder = new StringBuilder("hello")
+  println(builder.getClass)
 }
