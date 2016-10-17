97d6cc1d44f45a7f5e760350ea93e56968d08679
sin13cos14 <sin13cos14@gmail.com>
2016-10-17 21:10:55 +0800

    Scala type

---
+++ src/main/scala/day5/Generics.scala
@@ -0,0 +1,22 @@
+import java.util._
+
+/**
+  * Created by sin13cos14 on 16/10/17  21:01.
+  */
+class Generics {
+//  var list1: List[Int] = new ArrayList[Int]
+//  var list2 = new ArrayList[Int]
+//
+//  list2 add 1
+//  list2 add 2
+//
+//  var total = 0
+//  for (index <- 0 until list2.size()) {
+//    total += list2.get(index)
+//  }
+//  println(total)
+}
+
+// bad case
+//错误的实例, error: not found: value list2
+// total += list2.get(index)
