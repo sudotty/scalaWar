d29a7ff714223b7b89e29295111058a0cf467be0
sin13cos14 <sin13cos14@gmail.com>
2016-10-17 21:13:12 +0800

    Scala type

---
+++ src/main/scala/day5/Generics2.scala
@@ -0,0 +1,14 @@
+package day5
+
+import java.util._
+
+/**
+  * Created by sin13cos14 on 16/10/17  21:01.
+  */
+class Generics2 {
+  var list1 = new ArrayList[Int]
+  var list2 = new ArrayList
+
+  //list2 = list1
+  //上述语句编译错误
+}
