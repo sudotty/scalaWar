18104cc0bdb634c576233b3a66ac3ec432f17bf0
sin13cos14 <sin13cos14@gmail.com>
2016-10-18 09:30:47 +0800

    Scala type

---
+++ src/main/scala/day5/OptionExample.scala
@@ -0,0 +1,14 @@
+/**
+  * Created by sin13cos14 on 16/10/17  21:46.
+  */
+class OptionExample {
+  def commentOnPractice(input: String) = {
+    if (input == "test") Some("good") else None
+  }
+
+  for (input <- Set("test", "hack")) {
+    val comment = commentOnPractice(input)
+    println("input " + input + " comment " + comment.getOrElse("Found no comments"))
+  }
+
+}
